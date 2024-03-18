c26745df9dcd8a4ce9b40e095857a93375c93daa
Isoqjon Bobojonov <aizekjon@gmail.com>
2024-03-18 17:49:31 +0500

    check mobile headers

---
+++ app/Http/Middleware/CheckHeaders.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+use Exception;
+use Illuminate\Http\Request;
+use Symfony\Component\HttpFoundation\Response;
+
+class CheckHeaders
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param \Closure(\Illuminate\Http\Request): (\Symfony\Component\HttpFoundation\Response) $next
+     * @throws Exception
+     */
+    public function handle(Request $request, Closure $next): Response
+    {
+        $requiredHeaders = ['Device-Name', 'App-Version', 'Device-Platform', 'Fcm-Token'];
+
+        foreach ($requiredHeaders as $header) {
+            if (!$request->hasHeader($header)) {
+                throw new Exception("Missing required header: " . $header);
+            }
+        }
+
+        // Updating fcm token in case it's changed on mobile side
+        if (auth()->user() && auth()->user()->fcm_token != $request->header('Fcm-Token')) {
+            auth()->user()->update([
+                'fcm_token' => $request->header('Fcm-Token'),
+            ]);
+        }
+
+        return $next($request);
+    }
+}
